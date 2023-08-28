47ca33da4635e80e5880a9d6716622e2dafea16e
Patrick Huang <huangpatrick16777216@gmail.com>
2023-08-27 20:46:07 -0700

    Make tar, and rescale images.

--- worker/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Wrapper around CognitiveStudio CLI.
-"""
--- worker/worker.py
+++ worker/worker.py
@@ -14,15 +14,18 @@ File structure:
         |__ nerf0    # Some unique nerf name.
             ...    # Output of ns-train
     |__ status.json    # Metadata or status.
+|__ dataset0.tar.gz    # Created on demand.
 ...
 
 Each individual dataset (e.g. `run0`) is stored in our database as a `tar.gz`.
 """
 
+import cv2
 import json
 from pathlib import Path
 from subprocess import check_call
 
+STANDARD_RES = 400
 TMP_DIR = Path("/tmp/NerfNetWorker")
 
 
@@ -66,6 +69,23 @@ class Dataset:
     def write_status(self, status):
         (self.path / "status.json").write_text(json.dumps(status, indent=4))
 
+    def rescale_images(self, res: int = STANDARD_RES):
+        """
+        Uses cv2 to rescale all raw images.
+        Largest side will become `res`.
+        """
+        for file in self.raw_data_path.iterdir():
+            img = cv2.imread(str(file))
+            h, w = img.shape[:2]
+            if h > w:
+                new_h = res
+                new_w = int(res * w / h)
+            else:
+                new_w = res
+                new_h = int(res * h / w)
+            img = cv2.resize(img, (new_w, new_h))
+            cv2.imwrite(str(file), img)
+
     def run_colmap(self):
         """
         Runs ns-process-data on `raw_data`, saving output to `colmap_data`.
@@ -76,3 +96,19 @@ class Dataset:
             "--data", self.path / "raw_data",
             "--output-dir", self.path / "colmap_data",
         ], cwd=self.path)
+
+    def make_tar(self) -> Path:
+        """
+        Create tarball of this dataset.
+        Runs `tar`; this isn't a fast operation, so don't use it too often.
+        """
+        check_call([
+            "tar",
+            "-czf",
+            self.tmpdir / f"{self.id}.tar.gz",
+            self.id,
+        ], cwd=self.tmpdir)
+        tar_path = self.tmpdir / f"{self.id}.tar.gz"
+        assert tar_path.exists()
+
+        return tar_path
