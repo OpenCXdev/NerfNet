d70979a60dfbe726478bb4baa3fbf6dea80f1ef0
Joshua Ahn <joshuajahn2001@gmail.com>
2023-09-04 18:45:57 -0500

    Update app.py

--- worker/app.py
+++ worker/app.py
@@ -1,5 +1,9 @@
 """
 Celery app declaration.
+
+Update from Sept 4 meeting: considerations for using dramatiq over celeryq. 
+Documentation here https://dramatiq.io, supports a GUI that is also very helpful https://github.com/Bogdanp/dramatiq_dashboard. 
+Basically a more updated version of celeryq that also supports task prioritization.
 """
 
 import argparse
